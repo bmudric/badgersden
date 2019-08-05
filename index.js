d0fa004768d2a8bbd9b556736d3d88e1d5f08a95
Bojan Mudric <14099136+bmudric@users.noreply.github.com>
2019-08-05 20:05:37 +0200

    refactor to use bootstrap4-toggle

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 let express = require("express");
 let app = express();
 
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap4-toggle/js'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap4-toggle/css'));
+app.use('/', express.static('frontend'));
+
 let devices = require("./mdnsbrowser").devices;
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
-
-app.get("/devices", (req, res, next) => {
+app.use("/devices", (req, res, next) => {
   console.log("client requesting devices: ", devices);
   res.json(devices.toJSON());
 });
 
-app.use('/', express.static('frontend'));
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
