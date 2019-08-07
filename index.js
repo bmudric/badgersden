712b9270326bd78a613d7d373b94d552e6c91763
Bojan Mudric <14099136+bmudric@users.noreply.github.com>
2019-08-07 17:41:02 +0200

    clean up bootstrap imports

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import 'bootstrap';
-
 let express = require("express");
 let app = express();
 
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/', express.static('frontend'));
+
 let devices = require("./mdnsbrowser").devices;
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
-
-app.get("/devices", (req, res, next) => {
+app.use("/devices", (req, res, next) => {
   console.log("client requesting devices: ", devices);
   res.json(devices.toJSON());
 });
 
-app.use('/', express.static('frontend'));
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
