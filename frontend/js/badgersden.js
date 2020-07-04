43ae11cf80151b8be5ea840039ca1019c10cae71
Bojan Mudric <14099136+bmudric@users.noreply.github.com>
2020-07-04 15:19:24 +0200

    dynamically generate relay list

--- frontend/js/badgersden.js
+++ frontend/js/badgersden.js
@@ -1,5 +1,6 @@
-$(function() {
-  $('#relay01').change(function() {
+$(function () {
+  $('#relay01').change(function () {
+    console.log("Toggle...")
     if ($('#relay01').prop('checked')) {
       $.getJSON("http://192.168.178.151:80/on", setToggle)
     } else {
@@ -15,9 +16,28 @@ function setToggle(relayJson) {
 
 function refreshStatus() {
   return $.getJSON("http://localhost:3000/devices", function (devices) {
+    let dl = $("#deviceList").empty();
     $.each(devices, function (id, url) {
+      // append checkbox for relay in the list
+      let li = $("<li>").attr("class", "list-group-item").appendTo(dl);
+      let div = $("<div>").attr("class", "form-group").appendTo(li);
+      let h3 = $("<h3>").appendTo(div);
+      $("<label>").attr("for", id).text(id).appendTo(h3);
+      let toggle = $("<input>").attr("id", id).attr("type", "checkbox").appendTo(h3);
+      toggle.bootstrapToggle();
+
+      // apply the toggle function to the added relay
+      toggle.change(function () {
+        console.log("Toggle " + id);
+        if ($('#relay01').prop('checked')) {
+          $.getJSON("http://" + url + "/on", setToggle)
+        } else {
+          $.getJSON("http://" + url + "/off", setToggle)
+        }
+      });
       console.log("device: ", id, "url: ", url);
-    })
+    });
+    dl.hide().show();
   });
 }
 
