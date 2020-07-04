12a13e45c0345e9b858051ba21d04dd2f9ed866a
Bojan Mudric <14099136+bmudric@users.noreply.github.com>
2020-07-04 20:10:59 +0200

    fix event loops and fetching initial state

--- frontend/js/badgersden.js
+++ frontend/js/badgersden.js
@@ -1,15 +1,22 @@
-function setToggle(relayJson) {
-  let relayOn = Boolean(relayJson["relayIsOn"] === "true");
-  $("#relay01").prop("checked", relayOn).change();
+function setToggle(relayOn, target) {
+  if (relayOn !== $(target).prop('checked')) {
+    $(target).prop('checked', relayOn).change();
+  }
 }
 
-function initializeToggle(event) {
+function createSetToggle(target) {
+  return function (relayJson) {
+    setToggle(JSON.parse(relayJson).relayIsOn, target);
+  }
+}
+
+function handleToggle(event) {
   if ($(event.target).prop('checked')) {
     console.log("Toggle " + event.data.id + " on");
-    $.postJSON("http://" + event.data.url + "/on", setToggle)
+    $.post("http://" + event.data.url + "/on", createSetToggle(event.target))
   } else {
     console.log("Toggle " + event.data.id + " off");
-    $.getJSON("http://" + event.data.url + "/off", setToggle)
+    $.post("http://" + event.data.url + "/off", createSetToggle(event.target))
   }
 }
 
@@ -22,10 +29,21 @@ function refreshStatus() {
       let div = $("<div>").attr("class", "form-group").appendTo(li);
       let h3 = $("<h3>").appendTo(div);
       $("<label>").attr("for", id).text(id).appendTo(h3);
-      let toggle = $("<input>").attr("id", id).attr("type", "checkbox").appendTo(h3);
-      toggle.bootstrapToggle();
-      toggle.change({id: id, url: url}, initializeToggle);
-      console.log("device: ", id, "url: ", url);
+      let toggle = $("<input>").attr("id", id).attr("type",
+          "checkbox").appendTo(h3);
+      toggle.bootstrapToggle({
+        onstyle: "success"
+      });
+
+
+      // the initial state is fetched from the relay
+      $.getJSON("http://" + url + "/status", function (relayJson) {
+        setToggle(relayJson.relayIsOn, toggle);
+        console.log("device: ", id, "url: ", url, "on: ", toggle.prop('checked'));
+
+        // the checkbox change handler calls the relay API
+        toggle.change({id: id, url: url}, handleToggle);
+      });
     });
     dl.hide().show();
   });
